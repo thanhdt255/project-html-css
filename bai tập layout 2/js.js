5fa09c0bdea8af273d37e8ac1a178233839e142e
thanhdt255 <92931908+thanhdt255@users.noreply.github.com>
2021-10-26 09:46:49 +0700

    bat htmlcss

---
+++ bai tập layout 2/js.js
@@ -0,0 +1,9 @@
+// function ham() {
+//     var drop = document.getElementsByClassName('drop');
+//     var dropcontent = document.getElementsByClassName('dropcontent');
+
+
+
+//     drop[0].style.background = 'blue';
+
+// }
